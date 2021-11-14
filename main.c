5e40bc28c539ae0ead19bafcd638f66d5780cf8a
tehtnaz <47542657+tehtnaz@users.noreply.github.com>
2021-11-14 18:06:50 -0500

    Update to debug_options, icon src reorganized

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 
 * VECTOR 2 READING FROM PHYSICS OBJECTS IS BROKEN!!!
 
+* Ladders (just need to draw)
 * Allow for crates to give velocity to one another
 * Disappearing walls / portals
 * Multiple portal instance ppossibility
